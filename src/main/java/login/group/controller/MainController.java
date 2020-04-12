927aa8a25c1084cacfcd4a1762508bc0e4fc0a04
Loginoleg123 <travolta@i.ua>
2020-04-12 18:17:32 +0300

    first commit

---
+++ src/main/java/login/group/controller/MainController.java
@@ -0,0 +1,43 @@
+package login.group.controller;
+
+import login.group.model.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by User on 12.04.2020.
+ */
+@Controller
+public class MainController {
+
+    private static List<Person> persons = new ArrayList<Person>();
+
+    static {
+        persons.add(new Person("Bill", "Gates"));
+        persons.add(new Person("Steve", "Jobs"));
+    }
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+
+        String message = "Hello Spring Boot + JSP";
+
+        model.addAttribute("message", message);
+
+        return "index";
+    }
+
+    @RequestMapping(value = { "/personList" }, method = RequestMethod.GET)
+    public String viewPersonList(Model model) {
+
+        model.addAttribute("persons", persons);
+
+        return "personList";
+    }
+
+}
